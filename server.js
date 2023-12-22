e9601ba4183ce55e8e066f260302ba750d72470d
IrynaShashko <95182459+IrynaShashko@users.noreply.github.com>
2023-12-22 12:32:52 +0200

    fix

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require("./app.js");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Iryna:4lH54vZ9rQaQEyDd@cluster0.w9ju3xr.mongodb.net/massage?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connect success"), app.listen(3000);
+  })
+  .catch((error) => console.log(error.message));
